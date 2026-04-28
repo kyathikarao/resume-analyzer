9bead01c1023fede2d33ce03461940f6736176d0
Kyathika Rao <kyathikarao@gmail.com>
2026-04-28 22:21:44 +0530

    Fixed spaCy deployment issue

--- app/text_preprocessor.py
+++ app/text_preprocessor.py
@@ -9,7 +9,14 @@ nltk.download('punkt')
 nltk.download('stopwords')
 
 # Load spaCy model
-nlp = spacy.load("en_core_web_sm")
+import spacy
+
+try:
+    nlp = spacy.load("en_core_web_sm")
+except:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
 # Load stopwords
 stop_words = set(stopwords.words('english'))
